64d081eab2e0974eb0b61341b78240938500a753
Tiddo Langerak <tiddolangerak@gmail.com>
2013-04-26 09:37:55 +0200

    nutteloze bestanden gedelete

--- Assets/Code/XAppleTest.js
+++
@@ -1,9 +0,0 @@
-#pragma strict
-
-class XAppleTest extends UUnitTestCase {
-  @UUnitTest
-  function firstTest() {
-  	UUnitAssert.Equals( "one", XApple.returnOne() );
-  	Debug.Log("javascript test runs");
-  }
-}
