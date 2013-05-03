15e74d4fb4d77fba0ecc1b370ffc08d38405cc63
Tiddo Langerak <tiddolangerak@gmail.com>
2013-05-03 10:31:17 +0200

    Oops, extra bestanden vergeten

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+public class Logger
+{
+    public static void Log(string message)
+    {
+        Debug.Log(message);
+    }
+}
+
+#else
+
+
+using System;
+public class Logger {
+    public static void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+#endif
