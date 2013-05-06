6df7b4e8589358ab7d362d0b0e4921fb1f6cf06b
Martijn Dwars <ikben@martijndwars.nl>
2013-05-06 12:32:01 +0200

    Added PointsPowerup test

---
+++ Tests/PointsPowerupTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+class PointsPowerupTest
+{
+    [SetUp]
+    public void testSetup()
+    {
+        StateManager.Instance.score = 0;
+    }
+
+    [Test]
+    public void testInitialization()
+    {
+        PointsPowerup pp = new PointsPowerup();
+
+        Assert.AreEqual(0, StateManager.Instance.score);
+    }
+
+    [Test]
+    public void testCollisionSingle()
+    {
+        PointsPowerup pp = new PointsPowerup();
+        pp.Collision();
+
+        Assert.AreEqual(50, StateManager.Instance.score);
+    }
+
+    [Test]
+    public void testCollisionDouble()
+    {
+        PointsPowerup pp = new PointsPowerup();
+        pp.Collision();
+        pp.Collision();
+
+        Assert.AreEqual(100, StateManager.Instance.score);
+    }
+}
