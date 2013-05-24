92a5bad58ceb29631e3f8d63d440d660a1fce80f
Martijn Dwars <ikben@martijndwars.nl>
2013-05-24 10:11:09 +0200

    Fixed tests

--- Tests/ObstacleTest.cs
+++ Tests/ObstacleTest.cs
@@ -6,12 +6,52 @@ using Moq;
 public class ObstacleTest
 {
     [Test]
-    public void testObstacleCollision()
+    public void testPlayerDiesOnCollision()
     {
+        // Arrange
         var mockObstacleBehaviour = new Mock<IObstacleBehaviour>();
         Obstacle obstacle = new Obstacle(mockObstacleBehaviour.Object);
 
+        // Act
         obstacle.Collision();
+
+        // Assert
         Assert.AreEqual(StateManager.State.DEAD, StateManager.Instance.getCurrentState());
+    }
+
+    [Test]
+    public void testResetGame()
+    {
+        // Arrange
+        var mockObstacleBehaviour = new Mock<IObstacleBehaviour>();
+        mockObstacleBehaviour
+            .Expect(m => m.ReloadScene())
+            .Verifiable();
+
+        Obstacle obstacle = new Obstacle(mockObstacleBehaviour.Object);
+
+        // Act
+        obstacle.Collision();
+
+        // Assert
+        mockObstacleBehaviour.Verify(m => m.ReloadScene());
+    }
+
+    [Test]
+    public void testScoreAfterReset()
+    {
+        // Arrange
+        var mockObstacleBehaviour = new Mock<IObstacleBehaviour>();
+        mockObstacleBehaviour
+            .Expect(m => m.ReloadScene())
+            .Verifiable();
+
+        Obstacle obstacle = new Obstacle(mockObstacleBehaviour.Object);
+
+        // Act
+        obstacle.Collision();
+
+        // Assert
+        Assert.AreEqual(0, StateManager.Instance.score);
     }
 }
