6ea63a9d0d6adb3edeae5631dea52ce0149054f3
Niels Spruit <niellus16@hotmail.com>
2013-05-30 09:19:47 +0200

    Documented and formatted the util scripts

--- Assets/Scripts/Util/ITimer.cs
+++ Assets/Scripts/Util/ITimer.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Timers;
 
+/// <summary>
+/// Defines an interface for a timer. This interface is useful for testing purposes.
+/// (As you can use a mocked 'implementation' for this interface)
+/// </summary>
 public interface ITimer
 {
+    /// <summary>
+    /// Start the timer
+    /// </summary>
     void Start();
+
+    /// <summary>
+    /// Stop the timer
+    /// </summary>
     void Stop();
-    
+
+    /// <summary>
+    /// The time after which the timer expires
+    /// </summary>
     double Interval { get; set; }
+
+    /// <summary>
+    /// Event handler that is called when the timer expires
+    /// </summary>
     event ElapsedEventHandler Elapsed;
 }
--- Assets/Scripts/Util/TimerAdapter.cs
+++ Assets/Scripts/Util/TimerAdapter.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Timers;
 
+/// <summary>
+/// Implementation of the ITimer interface. The implementation is the same as in
+/// in the native System Timer. This adapter is useful for testing. (as it implements the interface)
+/// </summary>
 public class TimerAdapter : Timer, ITimer
 {
 }
